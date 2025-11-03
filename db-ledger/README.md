1dc0682d865822e196275d4d39f04a351db5fa2b
Goran Karlic <goran.karlic@gmail.com>
2025-11-03 09:11:35 +0100

    Add relational database example.

---
+++ db-ledger/README.md
@@ -0,0 +1,5 @@
+# SQL database ledger example
+
+This is an example of how to implement a simple ledger using a SQL database.
+
+Try it here: [https://www.db-fiddle.com/f/apoEA3KDjvjRf2v5BzWB3Y/1](https://www.db-fiddle.com/f/apoEA3KDjvjRf2v5BzWB3Y/1)
---
+++ db-ledger/ledger.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS ledger;
+
+CREATE TABLE IF NOT EXISTS ledger
+(
+    id INT UNIQUE,
+    sender INT,
+    recipient INT,
+    value INT
+);
+
+INSERT INTO ledger (id, sender, recipient, value) VALUES
+(1, 101, 102, 60),
+(2, 102, 103, 40),
+(3, 103, 101, 20);
+
+WITH Accounts AS
+(
+	SELECT DISTINCT sender AS account FROM Ledger
+	UNION
+	SELECT DISTINCT recipient AS account FROM Ledger
+),
+Sums AS
+(
+	SELECT
+	account,
+	(SELECT SUM(value) FROM ledger WHERE recipient = account) AS received,
+	(SELECT SUM(value) FROM ledger WHERE sender = account) AS sent
+	FROM Accounts
+)
+SELECT
+account,
+received,
+sent,
+received - sent FROM Sums;
